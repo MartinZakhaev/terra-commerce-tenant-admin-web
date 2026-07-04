1cb73d934df8eb7872381f7cb656b4f6bad4ab99
AndikaWahyuSyaputra <78039794+MartinZakhaev@users.noreply.github.com>
2026-07-04 18:07:55 +0700

    feat: add tenant admin application shell

---
+++ app/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <NuxtPage />
+</template>
+
+<style>
+:root {
+  color-scheme: light;
+  font-family: Inter, ui-sans-serif, system-ui, sans-serif;
+  background: #f6f7f9;
+  color: #18181b;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+</style>
